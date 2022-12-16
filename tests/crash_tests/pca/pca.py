185b18c657b8199d43e6fb833b1e474cdd2dac19
Nicolas <nico.faglin@gmail.com>
2022-12-17 01:22:53 +0200

    refactor(Test): end of tests on pca

--- tests/crash_tests/pca/pca.py
+++ tests/crash_tests/pca/pca.py
@@ -10,7 +10,6 @@ from lib.data_manager import DataManager
 dm = DataManager(symbol='MATICBUSD')
 dm.get_historical_klines(start_stop_day=(1, 31), kline_interval = '1h')
 df = dm.dataframe
-print(df)
 
 
 # PCA dataframe
@@ -19,4 +18,17 @@ for ma in ma_list:
     df[f'ma{ma}'] = TA.SMA(df, ma)
 
 df_pca = df[['close', 'ma5', 'ma10', 'ma20', 'ma50', 'ma100', 'ma200']].copy()
+df_pca = df_pca.dropna()
+df_pca = df_pca[['ma5', 'ma10', 'ma20', 'ma50', 'ma100', 'ma200']].apply(lambda x: x / df_pca['close'])
 print(df_pca)
+
+# PCA
+from sklearn.decomposition import PCA
+pca = PCA()
+pca.fit(df_pca)
+
+somme_proba = 0
+for col in range(len(df_pca.columns)):
+    somme_proba += pca.explained_variance_ratio_[col]
+    print(f'PC{col} : {pca.explained_variance_ratio_[col]} - {100*somme_proba:0.2f} %')
+    
