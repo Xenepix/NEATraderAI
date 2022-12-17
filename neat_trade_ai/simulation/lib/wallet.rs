4ace5e9f0fa3522dc7259c136d4a295355ee7379
Nicolas <nico.faglin@gmail.com>
2022-12-17 03:28:06 +0200

    refactor(Wallet): setup public methods

--- neat_trade_ai/simulation/lib/wallet.rs
+++ neat_trade_ai/simulation/lib/wallet.rs
@@ -1,16 +1,16 @@
 use std::time;
 
-enum OrderType { Buy, Sell }
+pub enum OrderType { Buy, Sell }
 
-struct Swap {
+pub struct Swap {
     /// Save an operation on wallet
-    order_type: OrderType,
-    amount: u128,
-    price: u128,
+    pub order_type: OrderType,
+    pub amount: u128,
+    pub price: u128,
     timestamp: u128,
 }
 impl Swap {
-    fn new(order_type: OrderType, amount: u128, price: u128) -> Self {
+    pub fn new(order_type: OrderType, amount: u128, price: u128) -> Self {
         Self {
             order_type,
             amount,
@@ -19,29 +19,29 @@ impl Swap {
         }
     }
 
-    fn get_str_timestamp(&self) -> String {
+    pub fn get_str_timestamp(&self) -> String {
         self.timestamp.format("%Y-%m-%d %H:%M:%S").to_string()
     }
 }
 
-struct Wallet {
+pub struct Wallet {
     /// Wallet for trading
     balance: u128,
     swaps: Vec<Swap>,
 }
 impl Wallet{
-    fn new(balance: u128) -> Self {
+    pub fn new(balance: u128) -> Self {
         Self {
             balance,
             swaps: Vec::new(),
         }
     }
 
-    fn get_balance(&self) -> u128 {
+    pub fn get_balance(&self) -> u128 {
         self.balance
     }
 
-    fn get_swaps(&self) -> &Vec<Swap> {
+    pub fn get_swaps(&self) -> &Vec<Swap> {
         &self.swaps
     }
 }
