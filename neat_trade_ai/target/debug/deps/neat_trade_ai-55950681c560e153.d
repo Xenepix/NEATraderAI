a647b182768f6814c1cc2bcc696be2536c870690
Nicolas <nico.faglin@gmail.com>
2022-12-17 03:05:47 +0200

    refactor(Rust): Setup rust environment

---
+++ neat_trade_ai/target/debug/deps/neat_trade_ai-55950681c560e153.d
@@ -0,0 +1,5 @@
+/home/nico/info_projects/NEATraderAI/neat_trade_ai/target/debug/deps/neat_trade_ai-55950681c560e153: src/main.rs
+
+/home/nico/info_projects/NEATraderAI/neat_trade_ai/target/debug/deps/neat_trade_ai-55950681c560e153.d: src/main.rs
+
+src/main.rs:
---
+++ neat_trade_ai/target/debug/neat_trade_ai.d
@@ -0,0 +1,1 @@
+/home/nico/info_projects/NEATraderAI/neat_trade_ai/target/debug/neat_trade_ai: /home/nico/info_projects/NEATraderAI/neat_trade_ai/src/main.rs
